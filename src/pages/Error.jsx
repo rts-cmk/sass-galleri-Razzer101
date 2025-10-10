25c45469c0dad744c3ca740a6fb4ae2a9d78260c
Razzer101 <Rasmus@anders-net.dk>
2025-10-10 13:10:33 +0200

    ErrorPageDone

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router";
+
 export default function Error(){
-    return
+    return(
+        <section className="error-section">
+            <h1 className="error-section__h1">Page Not Found</h1>
+            <Link className="error-section__link" to="/">Return to main page?</Link>
+        </section>
+    )
 }
