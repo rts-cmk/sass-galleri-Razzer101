33787d96b6b23abdba473c3dcbf9dd9bd9805cfa
Razzer101 <Rasmus@anders-net.dk>
2025-10-10 11:01:41 +0200

    ModalDone

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -4,7 +4,7 @@ import Gallery from "../components/Gallery"
 import imgData from "../assets/imgData.json"
 
 export default function Explore(){
-    const exploreImgs = imgData.food.sort().concat(imgData.lifestyle)
+    const exploreImgs = imgData.food.concat(imgData.lifestyle)
     
     return(
         <>
