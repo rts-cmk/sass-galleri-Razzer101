7a7f04b0c5e0eac19143682f77f2d05e40fa8b1c
Razzer101 <Rasmus@anders-net.dk>
2025-10-10 13:24:53 +0200

    RandomExploreImgs

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -4,8 +4,27 @@ import Gallery from "../components/Gallery"
 import imgData from "../assets/imgData.json"
 
 export default function Explore(){
-    const exploreImgs = imgData.food.concat(imgData.lifestyle, imgData.travel)
-    
+    const newImgData = new Array(
+        imgData.food[0],
+        imgData.food[1],
+        imgData.food[2],
+        imgData.food[3],
+        imgData.home[0],
+        imgData.home[1],
+        imgData.home[2],
+        imgData.home[3],
+        imgData.travel[0],
+        imgData.travel[1],
+        imgData.travel[2],
+        imgData.travel[3],
+        imgData.lifestyle[0],
+        imgData.lifestyle[1],
+        imgData.lifestyle[2],
+        imgData.lifestyle[3]
+    )
+    const exploreImgs = newImgData.sort(() => Math.random() - .5)
+    console.log(exploreImgs)
+
     return(
         <>
             <Header titleName="Explore"/>
